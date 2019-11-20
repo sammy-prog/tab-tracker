c505a51e7c503197228314b4278eef22ad0080b3
sammy-prog <44973046+sammy-prog@users.noreply.github.com>
2019-11-20 03:26:04 -0800

    setting up registration

--- server/src/app.js
+++ server/src/app.js
@@ -1,3 +1,17 @@
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const bodyParser = require('body-parser')
 
-console.log('hello');
-const express = require('express');
+const app = express()   
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.post('/register', (req, res) => {
+    res.send({
+        message: `hello ${req.body.email} you are registered!`
+    }) 
+})
+
+app.listen(process.env.PORT || 8081)
